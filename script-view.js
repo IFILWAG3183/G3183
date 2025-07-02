9aaaba6bc7e33360c3e2504f6580d8ba3be70ee3
IFILWAG3183 <15129821097@163.com>
2025-07-02 12:44:02 +0800

    Add files via upload

--- script-view.js
+++ script-view.js
@@ -1,12 +1,1 @@
-
-fetch('log.json')
-  .then(r => r.json())
-  .then(data => {
-    const container = document.getElementById('log-container');
-    data.forEach(entry => {
-      const div = document.createElement('div');
-      div.className = 'entry';
-      div.innerText = entry;
-      container.appendChild(div);
-    });
-  });
+// placeholder content for script-view.js
